ab88d4b4b365130766e6e083e7d1e2ada2c5da4b
mwaidesu <machariamwai@gmail.com>
2022-03-24 20:50:30 +0300

    Add footer and add scenery html

---
+++ css/js/scripts.js
@@ -0,0 +1,10 @@
+/* $("#myCarousel").carousel();
+
+
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+}); */
